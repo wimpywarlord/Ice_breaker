e3a1fdd1eb14fe39db030f624e5fa1d9c792e02f
wimpywarlord <wimpywarlord@gmail.com>
2020-08-06 00:37:52 +0530

    progress with peer

--- public/script.js
+++ public/script.js
@@ -1,7 +1,15 @@
 const socket = io("/")
+var peer = new Peer(undefined,{
+    host : '/',
+    port : '3001'
+});
 
-socket.emit("join-room",ROOM_ID ,10)
+myPeer.on('open', id => {
+    socket.emit("join-room",ROOM_ID ,id)
+})
+
+
 
 socket.on("user-connected",userId => {
-    console.log("user Connected")
+    console.log("user Connected", userId)
 })
