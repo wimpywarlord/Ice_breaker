56c6609cb9240400353b4db6102355faf65d937c
wimpywarlord <wimpywarlord@gmail.com>
2020-08-06 19:23:47 +0530

    all basic setup done

--- server.js
+++ server.js
@@ -1,34 +1,29 @@
-const express = require("express")
-const app = express();
-const server = require("http").Server(app)
-const io = require("socket.io")(server)
-const { v4:uuidV4 } =require('uuid');
-const { Socket } = require("dgram");
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-// console.log(uuidV4())
-
-
-app.get("/",(req,res) => {
-    res.redirect(`/${uuidV4()}`)
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:room',(req,res) => {
-    res.render('room', { roomId : req.params.room})
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
 
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
 
-io.on("connection", socket => (
-    socket.on("join-room",(roomId,userId) => {
-        console.log(userId,roomId)
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit("user-connected")
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
-))
+  })
+})
 
-
-server.listen(5000, function () {
-    console.log("SERVER 5000 HAS STARTED");
-});
+server.listen(3000)
